7865617be136ac5274fd66c6e4c67d4925f7f155
Reza <116279493+RezaKarimpour@users.noreply.github.com>
2023-12-16 13:58:34 +0330

    add course 2 cpp

---
+++ c++/Course 2/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+
+using namespace std;
+
+
+#define P 3.15;
+
+int main() 
+{
+	//int a = 18;
+	//float b = 18.5; 
+	//char r = 'R';
+	//string rb = "Root one ";
+
+	//long int u = 100000000;
+
+	int a, b, c, r = 12, l = 15;
+
+	//a = 22; 
+	//cout << "sene khod ra vared konid: "; 
+	//cin >> a ;
+	//string R;
+	//cout << "name khod ra vared koni";
+	//cin >> R;
+
+	//cout << endl <<  "sene shoma : " << a  << "name:" << R;
+
+
+	//const float P = 3.14;
+
+
+	cout << P;
+
+	return 0;
+
+}
