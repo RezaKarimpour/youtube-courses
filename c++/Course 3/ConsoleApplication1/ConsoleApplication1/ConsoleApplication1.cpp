8e1904404bdc3fb60bda9e655132c56a727f0e73
Reza <116279493+RezaKarimpour@users.noreply.github.com>
2023-12-30 11:29:38 +0330

    add course 4 cpp

---
+++ c++/Course 3/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{
+
+	int b = 12, a = 15 , c;
+	//c = b + a;
+	// b -= a;
+	// b *= a;
+	// b /= a;
+	// b %= a;
+	 //cout << b;
+
+	 //b != a;
+	 //b == a;
+	 //b >= a;
+	 //b <= a;
+
+	 //b--;
+	 //b++;
+	/*and &&
+		or || 
+		not !*/
+	 cout << b;
+
+	return 0;
+
+}
---
+++ c++/Course 4/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,132 @@
+
+
+
+
+
+
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{
+	//int a, b = 3 , c=5;
+	//if (a > b) {
+	//	cout << a;
+	//}else if(c>b) {
+	//	cout << c;
+	//}else {
+	//	cout << b << "bozorgta ast";
+	//}
+	//cout << "enter a number: ";
+	//cin >> a;
+
+	//if (a > c) {
+	//	cout << "yes";
+	//}
+
+	//if (a % 2 == 0) {
+	//	cout << "yes";
+	//}
+	//else {
+	//	cout << "no";
+	//}
+
+	//if (a==1) {
+	//	cout << "shanbe";
+	//}
+	//else if (a == 2) {
+	//	cout << "1 shanbe";
+	//}
+	//else if (a == 3) {
+	//	cout << "2 shanbe";
+	//}
+	//else if (a == 4) {
+	//	cout << "3 shanbe";
+	//}
+	//else if (a == 5) {
+	//	cout << "4 shanbe";
+	//}
+	//else if (a == 6) {
+	//	cout << "5 shanbe";
+	//}
+	//else if (a == 7) {
+	//	cout << "jome";
+	//}
+	//else {
+	//	cout << "nemidon!";
+	//}
+
+	//a, e, i, o, u
+
+	//char b;
+	//cout << "enter a chracter: ";
+	//cin >> b;
+
+	//if (b == 'a' || b == 'e' || b == 'i') {
+	//	cout << "yes";
+	//}
+	//else {
+	//	cout << "no";
+	//}
+
+
+	//if (b == 'a') {
+	//	cout << "yes";
+	//}
+	//else if (b == 'e') {
+	//	cout << "yes";
+	//}
+	//else if (b == 'i') {
+	//	cout << "yes";
+	//}
+	//else if (b == 'o') {
+	//	cout << "yes";
+	//}
+	//else if (b == 'u') {
+	//	cout << "yes";
+	//}
+	//else {
+	//	cout << "no";
+	//}
+
+
+	/*switch (a)
+	{
+	case 1:
+		cout << "shanbe";
+		break;
+	case '2':
+		cout << "yek shanbe";
+		break;
+
+	default:
+		cout << "no";
+		break;
+	}*/
+
+	//int x, y;
+	//x = 5;
+	//y = 10;
+
+	// (x > y) ? cout<< "yes" : cout <<"no";
+
+
+
+int a;
+cout << "enter a number: ";
+cin >> a;
+
+if (a>10 && a<100) {
+	a *= 2;
+	cout << a;
+}
+
+
+
+	return 0;
+
+}
